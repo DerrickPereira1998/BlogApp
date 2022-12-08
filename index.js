a14baf678e7bf8b59947b309a715270b989ff664
DerrickPereira1998 <derrickpereira1998@gmail.com>
2022-12-07 23:37:25 -0300

    derrick postagem setup

--- index.js
+++ index.js
@@ -94,6 +94,45 @@ require('./config/auth')(passport)
         })
     })
 
+    app.get('/addpostagem', (req,res) => {
+        Categoria.find().lean().then((categorias) => {
+            res.render('postagem/addpostagem', {categorias: categorias})
+        }).catch((erro) => {
+            req.flash('error_msg','Houve um erro ao recarregar o formulario')
+            res.redirect("/")
+        })
+    })
+
+    //Nova postagem
+    app.post('/novapostagem', (req,res) => {
+
+        var erros = []
+        if(req.body.categoria == '0'){
+            erros.push({texto: 'Categoria invalida, registre um categoria'})
+        }
+
+        if(erros.length > 0){
+            res.render('postagem/addpostagem', {erros:erros})
+        }
+        else{
+            const novaPostagem = {
+                titulo: req.body.titulo,
+                descricao: req.body.descricao,
+                conteudo: req.body.conteudo,
+                categoria: req.body.categoria,
+                slug: req.body.slug
+            }
+
+            new Postagem(novaPostagem).save().then(() => {
+                req.flash('success_msg', 'Postagem criada com sucesso')
+                res.redirect("/")
+            }).catch((erro) => {
+                req.flash('error_msg', 'Houve um erro durante o salvamento da postagem: '+erro)
+                res.redirect("/")
+            })
+        }
+    })
+
     app.get('/postagem/:titulo', (req,res) => {
         Postagem.findOne({titulo: req.params.titulo}).lean().then((postagem) => {
             if(postagem){
